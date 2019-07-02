7aa5e53283386aaf59530a380c10f8881c6688c2
Patrick Veverka <veverkap@users.noreply.github.com>
2019-07-02 12:54:18 -0400

    filessss

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-resource "null_resource" "example2" {
-  provisioner "local-exec" {
-    command = "ls -al"
-  }
-}
-
 resource "null_resource" "example3" {
   provisioner "local-exec" {
     command = "cat .terraform/terraform.tfstate"
